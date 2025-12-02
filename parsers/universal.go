2dacf8f63877227b7b56c03f5d7ad8a09e3cf2fb
vadimtoptunov <vtoptunov88@gmail.com>
2025-12-02 22:49:58 +0200

    Refactor UniversalParser by removing unused city-related functions
    - Eliminated GetCitiesForCountry, GetAllMajorCities, and ParseWithCities functions from UniversalParser.
    - Streamlined the code for improved maintainability and clarity.

--- parsers/universal.go
+++ parsers/universal.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"log"
 	"pricemap-go/models"
-	"pricemap-go/utils"
-	"time"
 )
 
 // UniversalParser is a meta-parser that uses multiple sources
@@ -52,55 +50,6 @@ func (up *UniversalParser) Parse(ctx context.Context) ([]models.Property, error)
 	
 	log.Printf("Universal parser collected %d properties from %d sources", 
 		len(allProperties), len(up.parsers))
-	
-	return allProperties, nil
-}
-
-// GetCitiesForCountry returns cities for a specific country
-func GetCitiesForCountry(country string) []utils.City {
-	return utils.GetCitiesByCountry(country)
-}
-
-// GetAllMajorCities returns all major cities worldwide
-func GetAllMajorCities() []utils.City {
-	return utils.GetMajorCities()
-}
-
-// ParseWithCities is a helper function for parsers that need to iterate over cities
-func ParseWithCities(ctx context.Context, parser Parser, cities []utils.City, 
-	parseFunc func(context.Context, utils.City) ([]models.Property, error)) ([]models.Property, error) {
-	
-	var allProperties []models.Property
-	
-	for _, city := range cities {
-		// Check context cancellation
-		select {
-		case <-ctx.Done():
-			return allProperties, ctx.Err()
-		default:
-		}
-		
-		properties, err := parseFunc(ctx, city)
-		if err != nil {
-			log.Printf("Error parsing %s, %s: %v", city.Name, city.Country, err)
-			continue
-		}
-		
-		// Set city information for all properties
-		for i := range properties {
-			properties[i].City = city.Name
-			properties[i].Country = city.Country
-			if properties[i].Latitude == 0 && properties[i].Longitude == 0 {
-				properties[i].Latitude = city.Latitude
-				properties[i].Longitude = city.Longitude
-			}
-		}
-		
-		allProperties = append(allProperties, properties...)
-		
-		// Rate limiting between cities
-		time.Sleep(1 * time.Second)
-	}
 	
 	return allProperties, nil
 }
